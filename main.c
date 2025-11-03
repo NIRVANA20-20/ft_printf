534186a93b2376b64b1d00c88ec8cd4236a5679b
oualid laizi <walidlaiz48@gmail.com>
2025-11-03 07:20:28 +0100

    Remove unused includes and main function

--- main.c
+++ main.c
@@ -1,16 +1,2 @@
-#include "ft_printf.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-int main(void)
-{ 
-
-    int nb = ft_printf("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
-    int nb1 =   printf("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
-    printf("\nft ==> %d ,, pr ==> %d\n",nb,nb1); 
-
-    return 0;
-}
 
 
