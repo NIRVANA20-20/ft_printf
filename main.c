bee30ddf666b2a9f539388e7e117fdb0ec98e53b
oualid laizi <walidlaiz48@gmail.com>
2025-11-04 19:45:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,13 @@
 
 
+#include "stdio.h"
+#include "unistd.h"
+#include "ft_printf.h"
+
+
+int main() {
+    char *s = "lalala ";
+    int x = printf(" s: h                %%%           jzxcbxcbzxzcbxcz  ");
+    int y = ft_printf(" a: h             %%%        jzxcbzxcbczxbcxz  ");
+    printf("\n{%d}  {%d}\n", x, y);
+}
