751ea8e0590eebb8e86324fb5bc9897aac142fbf
oualid laizi <walidlaiz48@gmail.com>
2025-12-02 09:16:57 +0100

    Update file header and clean up code
    Updated the timestamp in the file header and removed unused variable.

--- ft_putadrr.c
+++ ft_putadrr.c
@@ -6,7 +6,7 @@
 /*   By: olaizi <olaizi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 15:39:07 by olaizi            #+#    #+#             */
-/*   Updated: 2025/11/02 18:51:38 by olaizi           ###   ########.fr       */
+/*   Updated: 2025/11/03 09:27:28 by olaizi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,13 @@ static int	put_hex(unsigned long long nbr)
 {
 	char	*hexa;
 	int		count;
-	int		nbr_f;
 
 	count = 0;
 	hexa = "0123456789abcdef";
 	if (nbr != 0)
 	{
 		count += put_hex(nbr / 16);
-		nbr_f = count;
 		count += write(1, &hexa[nbr % 16], 1);
-		if(count < nbr_f)
-			return(-1);
 	}
 	return (count);
 }
